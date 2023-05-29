56618b920df5cfc5080f67a6a8deb83527aa4271
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-29 12:39:32 +0600

    add indexes

---
+++ src/components/navigation/index.ts
@@ -0,0 +1,5 @@
+export { default as Logo } from "./Logo";
+export { default as MenuItem } from "./MenuItem";
+export { default as NavigationBar } from "./NavigationBar";
+export { default as Search } from "./Search";
+export { default as UserMenu } from "./UserMenu";
