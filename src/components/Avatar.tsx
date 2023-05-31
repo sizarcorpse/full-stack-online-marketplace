1c9d34128d74a58c7d660d4b78b3689b663bdff7
Sizar Corpse <sizarcorpse@gmail.com>
2023-05-31 15:55:43 +0600

    update(component): add user image

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -2,8 +2,20 @@
 import Image from "next/image";
 import avatar from "/public/images/avatar-male.png";
 
-const Avatar = () => {
-  return <Image src={avatar} width={36} height={36} alt="Avatar" />;
+interface AvatarProps {
+  src?: string | null | undefined;
+}
+
+const Avatar: React.FC<AvatarProps> = ({ src }) => {
+  return (
+    <Image
+      src={src || avatar}
+      width={36}
+      height={36}
+      alt="Avatar"
+      className="rounded-full"
+    />
+  );
 };
 
 export default Avatar;
